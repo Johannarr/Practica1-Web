3e8b14865b5c5244a365e2ad21e959ef43118b3a
Johanna Rod <39653403+yohannarr@users.noreply.github.com>
2019-05-15 01:38:48 -0400

    Imagenes y forms

--- src/main/java/practica1/johanna/rodriguez/Main.java
+++ src/main/java/practica1/johanna/rodriguez/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
         //String url= JOptionPane.showInputDialog("Escribe una url completa, iniciando http...");//Entrada de url
-        String url = "https://www.pucmm.edu.do/";
+        String url = "https://www.pucmm.edu.do";
 
         try {
             Document doc = Jsoup.connect(url).get(); //Guarda el html de la pagina
@@ -26,6 +26,17 @@ public class Main {
             //Apartado B
             Elements parrafos= doc.getElementsByTag("p");
             System.out.println("La cantidad de parrafos del recurso es : " + parrafos.size());
+
+            //Apartado C
+            Elements imagenes = doc.select("p img");
+            System.out.println("La cantidad de imagenes dentro de parrafos del HTML es : " + imagenes.size());
+
+            //Apartado D
+            Elements formGet = doc.select("[method=get]");
+            System.out.println("La cantidad de formularios metodo GET del HTML es : " + formGet.size());
+
+            Elements formPost = doc.select("[method=post]");
+            System.out.println("La cantidad de formularios metodo POST del HTML es : " + formPost.size());
 
 
         } catch (IOException e) {
