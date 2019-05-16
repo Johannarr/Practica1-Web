7c4425f41e90b7ba4eb3e4d36114e8e7cfbfb1c0
Johanna Rod <39653403+yohannarr@users.noreply.github.com>
2019-05-16 02:24:35 -0400

    Validar url

--- src/main/java/practica1/johanna/rodriguez/Main.java
+++ src/main/java/practica1/johanna/rodriguez/Main.java
@@ -1,22 +1,36 @@
 package practica1.johanna.rodriguez;
 
+
+import org.apache.commons.validator.routines.UrlValidator;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import javax.swing.*;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        //String url= JOptionPane.showInputDialog("Escribe una url completa, iniciando http...");//Entrada de url
-        String url = "https://www.pucmm.edu.do";
+
+        Scanner scanner = new Scanner(System.in);
+        UrlValidator urlValidator = new UrlValidator();
+        //String url = "https://www.pucmm.edu.do";
+        String url;
+
+        do {
+            System.out.println("Introduzca una URL: ");
+            url = scanner.next();
+            if (urlValidator.isValid(url)){
+                System.out.println("La URL es Valida!");
+            }else {
+                System.out.println("La URL no es Valida, por favor introduzcala de nuevo.");
+            }
+        }while (!urlValidator.isValid(url));
+        scanner.close();
 
         try {
             Document doc = Jsoup.connect(url).get(); //Guarda el html de la pagina
@@ -25,20 +39,24 @@ public class Main {
             String cuerpo = conexion.body();
 
            //Apartado A
+            System.out.println("\nApartado A: ");
             //String[] lineas = doc.html().split("\n");
             //System.out.println("La cantidad de lineas del recurso es : " + lineas.length);
             int lineas = cuerpo.split("\n").length;
             System.out.println("La cantidad de lineas del recurso es : " + lineas);
 
             //Apartado B
+            System.out.println("\nApartado B: ");
             Elements parrafos= doc.getElementsByTag("p");
             System.out.println("La cantidad de parrafos del recurso es : " + parrafos.size());
 
             //Apartado C
+            System.out.println("\nApartado C: ");
             Elements imagenes = doc.select("p img");
             System.out.println("La cantidad de imagenes dentro de parrafos del HTML es : " + imagenes.size());
 
             //Apartado D
+            System.out.println("\nApartado D: ");
             Elements formGet = doc.select("form[method=get]");
             System.out.println("La cantidad de formularios metodo GET del HTML es : " + formGet.size());
 
@@ -46,11 +64,13 @@ public class Main {
             System.out.println("La cantidad de formularios metodo POST del HTML es : " + formPost.size());
 
             //Apartado E
+            System.out.println("\nApartado E: ");
             for (Element input : doc.getElementsByTag("input")) {
                 System.out.println(input);
             }
 
             //Apartado F
+            System.out.println("\nApartado F: ");
             for (Element form:doc.select("form[method=post]")) {
                 String  URL= form.absUrl("action");
                 Document doc1 = Jsoup.connect(URL)
